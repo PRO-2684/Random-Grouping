36d0cdb97f4446c2ae1e58558d242205cf76de6f
ihcnay <2435459530@qq.com>
2022-05-14 11:49:17 +0800

    examine

--- examine.py
+++ examine.py
@@ -28,7 +28,8 @@ def trans(orgin_list1, teamlist1):
     for team in teamlist1:
         teplist.append([])
         for person in team:
-            teplist[i1].append(re.split(r'[ ]', orgin_list1[int(person[2:5]) - 1]))
+            k1 = int((re.split(r'[ ]', person))[1])
+            teplist[i1].append(re.split(r'[ ]', orgin_list1[k1 - 1]))
         i1 += 1
     return teplist
 
@@ -48,25 +49,23 @@ def average_ability(team_list):
         tep_list = []
         for person in team:
             tep_list.append(int(person[3]))
-        aver_ability.append(statistics.mean(tep_list))
+        aver_ability.append(round(statistics.mean(tep_list), 2))
     return aver_ability
 
 
-# 计算各组成员的男女比例,队伍中无女生则比例为-1
+# 计算各组成员的男生比例
 def gender(team_list):
     sex_ratio = []
     for team in team_list:
         male = 0
-        female = 0
+        total = 0
         for person in team:
             if person[2] == '男':
                 male += 1
+                total += 1
             else:
-                female += 1
-        try:
-            sex_ratio.append('%.2f' % (male / female))
-        except:
-            sex_ratio.append(-1)
+                total += 1
+        sex_ratio.append(round(male / total, 2))
     return sex_ratio
 
 
@@ -78,8 +77,8 @@ def exam_dorm(team_list):
             if person[4] not in dorm:
                 dorm.append(person[4])
             else:
-                return 0
-    return 1
+                return False
+    return True
 
 
 result_file = input("请输入结果文件名:")
@@ -87,7 +86,7 @@ name_list = read(result_file)[1:]
 teamlist = [[]]
 k = 1
 for i in name_list:
-    if int(i[0]) == k:
+    if int((re.split(r'[ ]', i))[0]) == k:
         teamlist[k - 1].append(i)
     else:
         teamlist.append([])
@@ -101,8 +100,8 @@ exam_num(teamlist)
 print("各组平均能力" + str(aver))
 print("各组平均能力值的极差为" + str(max(aver) - min(aver)))
 sexradio = gender(teamlist)
-print("各组男女比例" + str(sexradio))
-if exam_dorm(teamlist) == 0:
-    print("组中存在同寝室成员")
-else:
+print("各组男生所占比例" + str(sexradio))
+if exam_dorm(teamlist):
     print("各组中不存在同寝室成员")
+else:
+    print("组中存在同寝室成员")
